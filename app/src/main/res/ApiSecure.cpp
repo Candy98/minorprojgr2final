7a24787de74bd4732e92010bf52515dbd4ba0866
candy <ryo98biswas@gmail.com>
2019-11-28 09:31:07 +0530

    splash added

---
+++ app/src/main/res/ApiSecure.cpp
@@ -0,0 +1,5 @@
+//
+// Created by candy on 23/11/19.
+//
+
+#include "ApiSecure.h"
---
+++ app/src/main/res/ApiSecure.h
@@ -0,0 +1,16 @@
+//
+// Created by candy on 23/11/19.
+//
+
+#ifndef MINORPROJGR2FINAL_APISECURE_H
+#define MINORPROJGR2FINAL_APISECURE_H
+
+
+
+class ApiSecure {
+
+};
+
+
+
+#endif //MINORPROJGR2FINAL_APISECURE_H
